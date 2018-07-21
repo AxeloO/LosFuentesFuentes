559dbbb09d8386d56de6ec9f9f9a32642d5cd70f
AxeloO <dj-dero-axel@hotmail.com>
2018-07-21 12:55:05 -0500

    Eliminando pruebas

--- WFFuentes/Program.cs
+++ WFFuentes/Program.cs
@@ -15,8 +15,7 @@ namespace WFFuentes
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.SetCompatibleTextRenderingDefault(false);         
         }
     }
 }
